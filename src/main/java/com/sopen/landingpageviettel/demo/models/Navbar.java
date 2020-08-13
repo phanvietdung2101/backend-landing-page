8f000f68c94e71895089f9eafefa883ad2f944e7
phanvietdung2101 <59188700+phanvietdung2101@users.noreply.github.com>
2020-08-13 09:58:07 +0700

    change image for navbar

--- src/main/java/com/sopen/landingpageviettel/demo/models/Navbar.java
+++ src/main/java/com/sopen/landingpageviettel/demo/models/Navbar.java
@@ -2,11 +2,9 @@ package com.sopen.landingpageviettel.demo.models;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
@@ -15,6 +13,7 @@ public class Navbar {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotEmpty
-    private String logo_src;
+    @NotNull
+    @ManyToOne
+    private Image logo_src;
 }
